dd554618da35c630d9e895c57101c5d25e6d4be1
Taylor Hartley <twh0008@auburn.edu>
2018-04-13 16:32:18 -0500

    Add proper functionality to gif command

--- commands/gif.js
+++ commands/gif.js
@@ -1,8 +1,8 @@
+require('dotenv').config();
+
 const fetch = require('node-fetch');
 
-require('dotenv').config();
-//Requires
-
+//Giphy API Key
 const key = process.env.GIPHY_API;
 
 
@@ -12,9 +12,15 @@ module.exports = {
     description: 'Displays random gif',
     execute(message, args) {
         fetch(`https://api.giphy.com/v1/gifs/random?api_key=${key}&tag=&rating=G`)
-        .then(stuff =>{
-            console.log(stuff);
-            message.channel.send(`Fetching...`);
+        .then(res =>{
+            return res.json();
+        })
+        .then(gif => {
+            message.channel.send(gif.data.embed_url);
+        })
+        .catch(err => {
+            message.channel.send(`There was an error. No gifs :(`);
+            console.log(err);
         });
         // .then(err => {
         //     console.log(err);
