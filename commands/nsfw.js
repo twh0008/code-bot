92541df8256f1a912ca5c9b5f2e8d88445f259a3
Taylor Hartley <twh0008@auburn.edu>
2018-04-20 18:24:53 -0500

    Add reddit wrapper for proper API use

--- commands/nsfw.js
+++ commands/nsfw.js
@@ -1,11 +1,23 @@
 require("dotenv").config();
 
+const snoowrap = require("snoowrap");
 const fetch = require("node-fetch");
 
 //Giphy API Key
 const KEY = process.env.GIPHY_API;
+const REDDIT_SECRET = process.env.REDDIT_SECRET;
+const REDDIT_USER = process.env.REDDIT_USER;
+const REDDIT_CLIENT = process.env.REDDIT_CLIENT;
+const REDDIT_REFRESH = process.env.REDDIT_REFRESH;
 
 //default rating
+
+const r = new snoowrap({
+  userAgent: "code-bot 0.1",
+  clientId: REDDIT_CLIENT,
+  clientSecret: REDDIT_SECRET,
+  refreshToken: REDDIT_REFRESH
+});
 
 module.exports = {
   name: "nsfw",
@@ -26,6 +38,12 @@ module.exports = {
     }
   },
   execute(message, args) {
+    if (!message.channel.nsfw) {
+      return message.channel.send(
+        "Channel must be marked as nsfw to use this command."
+      );
+    }
+
     if (this.extra) {
       //could add a count for the number of -* in args..
 
@@ -67,47 +85,30 @@ module.exports = {
     switch (input) {
       //RANDOM GIF
       case `gif`:
-        fetch(`https://www.reddit.com/r/Gif_sources/.json`)
-          //parseJSON
-          .then(parseJSON)
-          //send GIF through message
-          .then(findRandom)
-          .then(sendMessage)
-          //catch errors
+        var red = r.getSubreddit(`Gif_sources`).getRandomSubmission();
+        red
+          .then(result => {
+            var url = result.url;
+            return message.channel.send(url);
+          })
           .catch(err => {
-            console.log(err);
+            return message.channel.send("UH OH, something went wrong");
           });
         break;
       //NSFW Image
       case `t`:
-        fetch(`https://www.reddit.com/r/nsfw.json`)
-          //parseJSON
-          .then(parseJSON)
-          //send GIF through message
-          .then(findRandom) // return message.channel.send(data.data.embed_url);    //image = data.children[index].data.url
-          .then(sendMessage)
-          //catch errors
+        var red = r.getSubreddit(`nsfw`).getRandomSubmission();
+
+        red
+          .then(result => {
+            var url = result.url;
+            return message.channel.send(url);
+          })
           .catch(err => {
-            console.log(err);
-            return message.channel.send(`There was an error. No gifs :(`);
+            return message.channel.send("UH OH, something went wrong");
           });
-        break;
       default:
         break;
-    }
-
-    function parseJSON(res) {
-      return res.json().then(parsedData => {
-        return parsedData.data.children;
-      });
-    }
-    function findRandom(data) {
-      var randomIndex = Math.floor(Math.random() * 24 + 1);
-      return data[randomIndex].data.url;
-    }
-    function sendMessage(url) {
-      console.log(url);
-      return message.channel.send(url);
     }
   }
 };
