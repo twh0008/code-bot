a990c14dc4e370a17ef66c075575fd9bc6f881b3
Taylor Hartley <twh0008@auburn.edu>
2018-04-23 17:12:54 -0500

    Add clear command for channels marked nsfw

--- commands/clear.js
+++ commands/clear.js
@@ -7,11 +7,12 @@ module.exports = {
         message.channel.fetchMessages({ limit: 100 })
             .then(messages => {
                 amount = `${messages.size}`;
-                if ((amount >= 2 && amount <= 100) && (message.channel.parent.name === 'bots')) {
-                    message.channel.bulkDelete(amount, true)
+                if (amount >= 2 && amount <= 100 && (message.channel.parent.name === "bots" || message.channel.nsfw)) {
+                  message.channel
+                    .bulkDelete(amount, true)
                     .catch(err => {
-                        console.log(err);
-                        message.channel.send(err);
+                      console.log(err);
+                      message.channel.send(err);
                     });
                 }
             }).catch(err => {
